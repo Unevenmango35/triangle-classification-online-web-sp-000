d18de115189255bdbf147a7b8095648e0b0fb325
Rosie Choi <rosiechoimanso@gmail.com>
2021-01-13 21:00:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,18 @@ class Triangle
   end
 
   def kind
+    min, min2, max = [@a, @b, @c].sort
+    if min < 0 ||  min + min2 <= max
+      raise TriangleError
+    end
+
+    if @a == @b && @b == c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
   end
 
 end
